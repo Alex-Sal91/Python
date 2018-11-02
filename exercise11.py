57b288a093031642b72b3be48e036d2f3d0b8928
Alex-Sal91 <alexandersalako@outlook.com>
2018-11-02 10:44:55 +0000

    first commit

---
+++ exercise11.py
@@ -0,0 +1,17 @@
+print("How old are you?")
+age  = raw_input()
+print("How tall are you?")
+height = raw_input()
+print("How much do you weigh?")
+weight = raw_input()
+
+print("So, you're {} years old, {} tall and weigh {}lbs.".format(age, height, weight))
+
+print("What is your name?")
+name = raw_input()
+print("Where are you from?")
+location = raw_input()
+print("What language do you speak?")
+lang = raw_input()
+
+print("My name is {}, I'm from {} and I speak {}".format(name, location, lang))
---
+++ exercise12.py
@@ -0,0 +1,5 @@
+name = raw_input("What's your name? ").capitalize()
+age = raw_input("How old are you? ").capitalize()
+favourite_language = raw_input("What's your favourite programming language? ").capitalize()
+
+print("My name is {}, I'm {} years old and my programming language of choice is {}. ".format(name, age, favourite_language))
