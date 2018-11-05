6aae8b7fe7d4ecd6578947e0bba79a853ca4cc4d
Alex-Sal91 <alexandersalako@outlook.com>
2018-11-05 03:00:13 +0000

    Account class complete

--- Bank Application/Bank Account.py
+++ Bank Application/Bank Account.py
@@ -2,4 +2,18 @@ class Account:
 
     def __init__(self, account_num, opening_deposit):
         self.account_num = account_num
-        self.opening_deposit = opening_deposit
+        self.balance = opening_deposit
+
+    def __str__(self):
+        return  "{}".format(self.balance)
+
+    def deposit(self, deposit_amount):
+        self.balance += deposit_amount
+
+    def withdraw(self, withdraw_amount):
+        if self.balance >= withdraw_amount
+            self.balance -= withdraw_amount
+        else:
+            return "Insufficient Funds"
+
+
