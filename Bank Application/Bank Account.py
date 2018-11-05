ea5084970e37fd8446b8b9f8739007aa7fdcd35d
Alex-Sal91 <alexandersalako@outlook.com>
2018-11-05 03:44:15 +0000

    implemented savings and business accounts

--- Bank Application/Bank Account.py
+++ Bank Application/Bank Account.py
@@ -16,4 +16,36 @@ class Account:
         else:
             return 'Insufficient Funds'
 
+class Current(Account):
+    def __int__(self, account_num, opening_deposit):
 
+        super(account_num, opening_deposit).__init__()
+
+    #Define a __str__ method that returns a string specific to Current accounts
+    def __str__(self):
+        return 'Current Account: {}\n Balance: {}'.format(self.account_num, Account.__str__(self))
+
+
+x = Current(44556, 17556)
+print(x)
+
+
+print(x.withdraw(15000))
+print(x.balance)
+
+class Savings(Account):
+    def __int__(self, account_num, opening_deposit):
+
+        super(account_num, opening_deposit).__init__()
+
+    def __str__(self):
+        return 'Savings Account: {}\n Balance: {}'.format(self.account_num, Account.__str__(self))
+
+
+class Business(Account):
+    def __int__(self, account_num, opening_deposit):
+
+        super(account_num, opening_deposit).__init__()
+
+    def __str__(self):
+        return 'Business Account: {}\n Balance: {}'.format(self.account_num, Account.__str__(self))
